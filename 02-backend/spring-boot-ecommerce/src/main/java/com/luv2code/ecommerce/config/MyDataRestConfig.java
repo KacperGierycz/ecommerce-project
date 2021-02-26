c8d5ab112ce93b78f90bbb0bee4629694149c701
Kacper <kacpergierycz@gmail.com>
2021-02-26 14:36:50 +0100

    expose the Id's for Categories using new(getMetamodel) lots of rearaging from array to list .ect.

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -2,15 +2,27 @@ package com.luv2code.ecommerce.config;
 
 import com.luv2code.ecommerce.entity.Product;
 import com.luv2code.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.core.mapping.HttpMethods;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entitityMenager;
+
+    @Autowired MyDataRestConfig(EntityManager theEntityManager){
+        entitityMenager=theEntityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -28,6 +40,31 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // call the internal helper method
+         exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        // expose entity ids
+        //
+
+        // -get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities=entitityMenager.getMetamodel().getEntities();
+
+        // create a array list of the entity types
+        List<Class> entityClasses= new ArrayList<>();
+
+        // get the entitys type from the Entitys
+        for ( EntityType tempEntityType : entities){
+        entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // expose the entity ids for the array of entity types
+        Class[] domainTypes=entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+
 
     }
 }
