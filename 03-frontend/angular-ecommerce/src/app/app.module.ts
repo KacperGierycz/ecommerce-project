50a1683e927dd0189f7853348a4a14be5354f2f2
Kacper <kacpergierycz@gmail.com>
2021-02-26 16:06:05 +0100

    app modules new PCMenuComp, ng class product-category,  ng component p-c{list-p-c.JSON.stringify), productservice new getC, html new menu *ngFor="..";

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -8,6 +8,7 @@ import {HttpClientModule} from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
 import{ Routes, RouterModule} from '@angular/router';
+import { ProductCategoryMenuComponent } from './component/product-category-menu/product-category-menu.component';
 
 const routes: Routes = [
   {path: 'category/:id', component: ProductListComponent},
@@ -21,7 +22,8 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductCategoryMenuComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
