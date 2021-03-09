3c84f22b7a9b99bd98a01d51abc2d3888cc5eaf3
Kacper <kacpergierycz@gmail.com>
2021-03-09 10:12:12 +0100

    forms of billing shipping card groups with copy if checked and review orde

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -10,6 +10,9 @@ export class CheckoutComponent implements OnInit {
 
   checkoutFormGroup: FormGroup;
 
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
@@ -19,9 +22,36 @@ export class CheckoutComponent implements OnInit {
         firstName: [''],
         lastName: [''],
         email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street:[],
+        city:[],
+        state:[],
+        country:[],
+        email:[],
+        zipCode:[],
+
+      }),
+      billingAddress: this.formBuilder.group({
+        street:[],
+        city:[],
+        state:[],
+        country:[],
+        email:[],
+        zipCode:[],
+
+      }),
+      creditCard: this.formBuilder.group({
+        cardType:[],
+        nameOnCard:[],
+        cardNumber:[],
+        securityCode:[],
+        expirationMonth:[],
+        expirationYear:[],
+
       })
 
-    })
+    });
 
   }
 
@@ -30,6 +60,16 @@ export class CheckoutComponent implements OnInit {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer').value);
     console.log("email is: "+ this.checkoutFormGroup.get('customer').value.email);
+  }
+
+  copyShippingAddressToBillingAddress(event){
+    if(event.target.checked){
+      this.checkoutFormGroup.controls.billingAddress
+        .setValue(this.checkoutFormGroup.controls.shippingAddress.value)
+    }
+    else{
+      this.checkoutFormGroup.controls.billingAddress.reset();
+    }
   }
 
 }
