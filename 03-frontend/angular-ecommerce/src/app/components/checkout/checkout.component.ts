4da33bb37af5ed987e209d8d1f33c0a6038e4fb2
Kacper <kacpergierycz@gmail.com>
2021-03-12 13:59:11 +0100

    billing

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -68,11 +68,26 @@ export class CheckoutComponent implements OnInit {
 
       }),
       billingAddress: this.formBuilder.group({
-        street:[''],
-        city:[''],
-        state:[''],
-        country:[''],
-        zipCode:[''],
+        street:new FormControl('',
+        [Validators.required, 
+        Validators.minLength(2),
+        Luv2ShopValidators.notOnlyWhitespaces]),
+
+        city:new FormControl('',
+        [Validators.required, 
+        Validators.minLength(2),
+        Luv2ShopValidators.notOnlyWhitespaces]),
+
+        state:new FormControl('',
+        [Validators.required]),
+
+        country:new FormControl('',
+        [Validators.required]),
+
+        zipCode:new FormControl('',
+        [Validators.required, 
+        Validators.minLength(2),
+        Luv2ShopValidators.notOnlyWhitespaces]),
 
       }),
       creditCard: this.formBuilder.group({
@@ -147,6 +162,13 @@ export class CheckoutComponent implements OnInit {
   get shippingAddressState() { return this.checkoutFormGroup.get('shippingAddress.state');}
   get shippingAddressZipCode() { return this.checkoutFormGroup.get('shippingAddress.zipCode');}
   get shippingAddressCountry() { return this.checkoutFormGroup.get('shippingAddress.country');}
+
+  get billingAddressStreet() { return this.checkoutFormGroup.get('billingAddress.street');}
+  get billingAddressCity() { return this.checkoutFormGroup.get('billingAddress.city');}
+  get billingAddressState() { return this.checkoutFormGroup.get('billingAddress.state');}
+  get billingAddressZipCode() { return this.checkoutFormGroup.get('billingAddress.zipCode');}
+  get billingAddressCountry() { return this.checkoutFormGroup.get('billingAddress.country');}
+
 
   copyShippingAddressToBillingAddress(event){
     if(event.target.checked){
