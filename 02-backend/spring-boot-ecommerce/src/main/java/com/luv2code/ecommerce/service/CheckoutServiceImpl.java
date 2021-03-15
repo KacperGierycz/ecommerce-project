f4dfbce413a781eb754bb0ca9c345f35c1f84082
Kacper <kacpergierycz@gmail.com>
2021-03-15 14:58:51 +0100

    getting from purchase saving customer,order, addreses, items in REPO

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
@@ -3,10 +3,13 @@ package com.luv2code.ecommerce.service;
 import com.luv2code.ecommerce.dao.CustomerRepository;
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
+import com.luv2code.ecommerce.entity.Customer;
 import com.luv2code.ecommerce.entity.Order;
+import com.luv2code.ecommerce.entity.OrderItem;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Set;
 import java.util.UUID;
 
 @Service
@@ -31,16 +34,22 @@ public class CheckoutServiceImpl implements CheckoutService {
         order.setOrderTrackingNumber(orderTrackingNumber);
 
         // populate order with orderItem
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(item -> order.add(item));
 
-        // populate order with billingAddress and shippingAddress
+        // populate order with billingAddress and shippingAddres
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
 
         // populate customer with order
+        Customer customer =purchase.getCustomer();
+        customer.add(order);
 
         // save to the database
+        customerRepository.save(customer);
 
         // return a response
-
-        return null;
+        return new PurchaseResponse(orderTrackingNumber);
 
     }
 
