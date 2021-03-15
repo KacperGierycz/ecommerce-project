8b05c7d6f04cde86e038912b05c06d57a51fabab
Kacper <kacpergierycz@gmail.com>
2021-03-15 14:26:34 +0100

    to the JSP the real work generate UUID in CheckoutS

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
@@ -3,7 +3,11 @@ package com.luv2code.ecommerce.service;
 import com.luv2code.ecommerce.dao.CustomerRepository;
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
+import com.luv2code.ecommerce.entity.Order;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.UUID;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
@@ -16,7 +20,56 @@ public class CheckoutServiceImpl implements CheckoutService {
     }
 
     @Override
+    @Transactional
     public PurchaseResponse placeOrder(Purchase purchase) {
+
+        // tretrieve the order info from dto
+        Order order = purchase.getOrder();
+
+        // generate tracking number
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        // populate order with orderItem
+
+        // populate order with billingAddress and shippingAddress
+
+        // populate customer with order
+
+        // save to the database
+
+        // return a response
+
         return null;
+
+    }
+
+    private String generateOrderTrackingNumber() {
+
+        // generate a random UUID number (UUID version-4)
+        // for details check: wiki universal unique identifier
+
+        return UUID.randomUUID().toString();
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
