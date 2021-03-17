a98e3ecefb7f0f444ad8c9e176d74b38a6c4bb05
Kacper <kacpergierycz@gmail.com>
2021-03-17 11:59:49 +0100

    do authorisation routing and imports

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import {HttpClientModule} from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
-import{ Routes, RouterModule} from '@angular/router';
+import{ Routes, RouterModule, Router} from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
@@ -20,7 +20,28 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { LoginStatusComponent } from './components/login-status/login-status.component';
 
+import {
+OKTA_CONFIG,
+OktaAuthModule,
+OktaCallbackComponent
+} from '@okta/okta-angular';
+
+import myAppConfig from './config/my-app-config';
+
+const oktaConfig = Object.assign({
+  onAuthRequired: (injector) =>{
+    const router = injector.get(Router);
+
+    // Redirect the user to your custom login page
+    router.cavigate(['/login']);
+  }
+
+}, myAppConfig.oidc);
+
 const routes: Routes = [
+  {path: 'login/callback', component: OktaCallbackComponent},
+  {path: 'login', component:LoginComponent},
+
   {path: 'checkout', component:CheckoutComponent},
   {path: 'cart-details', component:CartDetailsComponent},
   {path: 'products/:id', component:ProductDetailsComponent},
