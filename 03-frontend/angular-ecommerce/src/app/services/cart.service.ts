4e2637957dd40eac525fc0d5fdaa3eaf5f6a576e
Kacper <kacpergierycz@gmail.com>
2021-03-18 11:54:13 +0100

    session/local storage/retrieve of cart iteams

--- 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -13,7 +13,23 @@ export class CartService {
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number>=new BehaviorSubject<number>(0);
 
-  constructor() {  }
+  storage: Storage = localStorage;
+
+  constructor() {
+
+    // read data from storage 
+
+    let data = JSON.parse(this.storage.getItem('cartItems'));
+
+    if (data != null){
+      this.cartItems=data
+
+      // compute totals based on the data that is read from storage
+      this.computeCartTotals();
+
+    }
+
+    }
 
   addToCart(theCartItem: CartItem){
   // check if we already hava the item in the cart
@@ -57,10 +73,18 @@ this.computeCartTotals();
   this.totalPrice.next(totalPriceValue);
   this.totalQuantity.next(totalQuantityValue);
 
-// log cart data just for debugging purposes
-this.logCartData(totalPriceValue, totalQuantityValue);
+  // log cart data just for debugging purposes
+  this.logCartData(totalPriceValue, totalQuantityValue);
+
+    // persist the cart data
+    this.persistCartItems();
 
   }
+
+  persistCartItems(){
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
+  }
+
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
     console.log('Content of the cart');
     for (let tempCartItem of this.cartItems){
