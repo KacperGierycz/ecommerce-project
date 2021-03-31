5a65bbcd23e46c96002ae48c093990272f5a2494
Kacper <kacpergierycz@gmail.com>
2021-03-31 14:38:11 +0200

    email retrieval and populate in form

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -33,6 +33,8 @@ export class CheckoutComponent implements OnInit {
   shippingAddressStates: State[]=[];
   billingAddressStates: State[]=[];
 
+  storage: Storage= sessionStorage;
+
   constructor(private formBuilder: FormBuilder,
               private luv2shopFormService:Luv2ShopFormService,
               private cartService: CartService,
@@ -43,6 +45,9 @@ export class CheckoutComponent implements OnInit {
   
     this.reviewCartDetails();
 
+    // read the user's email address from browser storage
+    const theEmail=JSON.parse(this.storage.getItem('userEmail'));
+
     this.checkoutFormGroup=this.formBuilder.group({
       customer:this.formBuilder.group({
         firstName: new FormControl('',
@@ -50,7 +55,7 @@ export class CheckoutComponent implements OnInit {
         Validators.minLength(2),
         Luv2ShopValidators.notOnlyWhitespaces]),
 
-        lastName: new FormControl('',[Validators.required,Validators.minLength(2),Luv2ShopValidators.notOnlyWhitespaces]),
+        lastName: new FormControl(theEmail,[Validators.required,Validators.minLength(2),Luv2ShopValidators.notOnlyWhitespaces]),
         email: new FormControl('',
                     [Validators.required,
                        Validators.pattern('^[A-Za-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,6}$')])
