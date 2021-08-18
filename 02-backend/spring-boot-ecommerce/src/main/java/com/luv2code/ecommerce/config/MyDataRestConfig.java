e25f0c1e1366aecc175f187a75e0b227382268ac
KacperGierycz <kacpergierycz@gmail.com>
2021-08-18 14:45:02 +0200

    ProductRepository new method with automagic name search

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -45,7 +45,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
          exposeIds(config);
 
          // configure cors mapping
-        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
+        cors.addMapping(config.getBasePath()  + "/**").allowedOrigins(theAllowedOrigins);
 
     }
 
